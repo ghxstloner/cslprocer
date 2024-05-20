51ee2b6ba52d736ab73e1802aa34f1243b207f84
Yoiner Moreno <yoinerdeyhan@gmail.com>
2024-05-20 02:54:55 -0500

    🚀 [INIT] init CSLProCer proyect

---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#818CF8",
+          "100": "#818CF8",
+          "200": "#818CF8",
+          "300": "#818CF8",
+          "400": "#818CF8",
+          "500": "#818CF8",
+          "600": "#818CF8",
+          "700": "#818CF8",
+          "800": "#818CF8",
+          "900": "#818CF8",
+          "950": "#818CF8"
+        }      
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
+  }
+}
